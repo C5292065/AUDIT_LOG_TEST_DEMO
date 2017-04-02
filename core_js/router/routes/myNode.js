2b4690540ce5319b201df037bf36695f39e1ac37
XSA_DEV <XSA_DEV@unknown>
2017-04-02 12:43:32 +0000

    2017_04_02_12_45

--- core_js/router/routes/myNode.js
+++ core_js/router/routes/myNode.js
@@ -2,6 +2,7 @@
 "use strict";
 var express = require("express");
 var async = require("async");
+var yahooFinance = require("yahoo-finance");
 var WebSocketServer = require("ws").Server;
 
 module.exports = function(server) {
@@ -11,34 +12,6 @@ module.exports = function(server) {
 		res.send("Hello World Node.js");
 	});
 	
-	app.get("/test001", function(req, res) {
-		var client = req.db;
-		async.waterfall([
-			function prepare(callback) {
-				client.prepare("select PHONENUMBER from \"MD.BusinessPartner\" ",
-					function(err, statement) {
-						callback(null, err, statement);
-					});
-			},
-			function execute(err, statement, callback) {
-				statement.exec([],
-					function(execErr, results) {
-						callback(null, execErr, results);
-					});
-			},
-			function response(err, results, callback) {
-				if (err) {
-					res.type("text/plain").status(500).send("ERROR: " + err.toString());
-					return;
-				} else {
-					var result = JSON.stringify({ Objects: results });
-					res.type("application/json").status(200).send(result);
-				}
-				callback();
-			}
-		]);
-	});
-
 	app.get("/example1", function(req, res) {
 		var client = req.db;
 		client.prepare("select SESSION_USER from \"DUMMY\" ",
@@ -92,6 +65,24 @@ module.exports = function(server) {
 			}
 		]);
 	});
+	
+	app.get("/yahoo_stocks_live", function(req, res) {
+
+		yahooFinance.historical({
+			symbol: 'AAPL',
+			from: '2012-01-01',
+			to: '2012-12-31'
+		}, function (err, quotes) {
+			if (err) {
+					res.type("text/plain").status(500).send("ERROR: " + err.toString());
+					return;
+				} else {
+					var result = JSON.stringify({ Objects: quotes });
+					res.type("application/json").status(200).send(result);
+				}
+		});
+	});
+	
 
 	return app;
 };
