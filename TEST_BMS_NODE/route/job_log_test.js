e3dcc55dd3454624e7aadc0c9f08caa81efe3a56
Sailen2010@ <jinalkumarppatel@gmail.com>
2019-08-19 09:02:33 +0200

    final

--- TEST_BMS_NODE/route/job_log_test.js
+++ TEST_BMS_NODE/route/job_log_test.js
@@ -9,16 +9,14 @@ module.exports = router.get("/", function(req, res) {
 			var thisApp = JSON.parse(process.env.VCAP_APPLICATION);
 			var thisAppURI = thisApp.full_application_uris[0];
 		
-			var final_url = thisAppURI+"/get";
-			
-			
+			var final_url = thisAppURI+"/jobs/{6}";
+			console.log(final_url);
 			request(final_url, { json: true }, function (err, res1, body){
   if (err) { return console.log(err); }
-  console.log(body.url);
-  res.send('success');
+  console.log(body); 
 });
-
-res.send('success');
+res.send(final_url);
+res.send("success2");
 });
 
 // module.exports = router;
