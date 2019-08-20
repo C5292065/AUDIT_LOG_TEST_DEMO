35f0b781fb30c53ba45e4e27d0d732484838427a
jinalkumarppatel@gmail.com <jinalkumarppatel@gmail.com>
2019-08-20 12:54:09 +0200

    final xsjs to node

--- core_js/lib/index.xsjs
+++ core_js/lib/index.xsjs
@@ -1,3 +1,19 @@
-$.response.contentType = "text/plain";
+// $.response.contentType = "text/plain";
 
-$.response.setBody("Hello World");
+// $.response.setBody("Hello World");
+
+ var dest = $.net.http.readDestination("EXTERNAL_HTTP");
+
+var client = new $.net.http.Client();
+var req = new $.web.WebRequest($.net.http.GET, "/node");
+client.request(req, dest);
+
+var response = client.getResponse();
+
+console.log("response " + response.status);
+
+var body = null;
+if (response.body) {
+    body = response.body.asString();
+    console.log(body);
+}
--- core_js/router/routes/myNode.js
+++ core_js/router/routes/myNode.js
@@ -1,16 +1,18 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, newcap:0*/
 "use strict";
 var express 		= require("express");
-var async			= require("async");
-var WebSocketServer = require("ws").Server;
 
-module.exports = function(server) {
+
+module.exports = function() {
 	var app = express.Router();
 
 	app.get("/", function(req, res) {
-		res.send("Hello World Node.js");
+		
+		var logger = req.loggingContext.getLogger('/Application');
+  //var tracer = req.loggingContext.getTracer(__filename);
+			logger.error('this is by jinal from node app');
+			res.send('success');
 	});
-
 
 	return app;
 };
