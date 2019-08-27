39a192a64f59991dfcfe372b0ef359ab440d9ad6
sarina2010 <50543657+sarina2010@users.noreply.github.com>
2019-08-27 16:00:51 +0530

    Update job_log_test.js

--- TEST_BMS_NODE/route/job_log_test.js
+++ TEST_BMS_NODE/route/job_log_test.js
@@ -1,22 +1,38 @@
-var express = require('express');
-var request = require('request');
-var router = express.Router();
+/*eslint no-console: 0, no-shadow: 0, new-cap: 0*/
+"use strict";
 
+var express = require("express");
+var xsenv = require("@sap/xsenv");
+var jobsc = require("@sap/jobs-client");
 
-module.exports = router.get("/", function(req, res) {
- 
-			// get the full URI of this app
-			var thisApp = JSON.parse(process.env.VCAP_APPLICATION);
-			var thisAppURI = thisApp.full_application_uris[0];
-		
-			var final_url = thisAppURI+"/jobs/{6}";
-			console.log(final_url);
-			request(final_url, { json: true }, function (err, res1, body){
-  if (err) { return console.log(err); }
-  console.log(body); 
-});
-res.send(final_url);
-res.send("success2");
-});
+module.exports = function() {
+                var app = express.Router();
 
-// module.exports = router;
+                // setup connection to job scheduler REST API
+                var jobOptions = xsenv.getServices({
+                               jobs: {
+                                               tag: "jobscheduler"
+                               }
+                });
+                var schedulerOptions = {
+                               // name:'myJob',
+                               jobId:'106',
+                               baseURL: jobOptions.jobs.url,
+                               user: jobOptions.jobs.user,
+                               password: jobOptions.jobs.password,
+                               timeout: 15000
+                };
+                var scheduler = new jobsc.Scheduler(schedulerOptions);
+                
+                app.get("/fetch", function(req, res) {
+                                               // scheduler.fetchJob(schedulerOptions, function(err, body) {
+                                               scheduler.fetchJobSchedules(schedulerOptions, function(err, body) {
+                                                               if (err) {
+                                                                               res.status(500).json(err); 
+                                                               } else {
+                                                                               res.status(200).json(body);
+                                                               }
+                                               });
+                });
+                return app;
+};
